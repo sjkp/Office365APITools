d88981d595a1bdfc22d80540281f5239e0cf1986
Simon Pedersen <mail@sjkp.dk>
2014-06-24 22:07:38 +0200

    Initial commit

---
+++ Office365APIToolsSample/Controllers/OneDriveController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Office365APIToolsSample.Controllers
+{
+    public class OneDriveController : Controller
+    {
+        // GET: OneDrive
+        public async Task<ActionResult> Index()
+        {
+
+            ViewBag.Files = await MyFilesApiSample.GetMyFiles();
+            return View();
+        }
+    }
+}
